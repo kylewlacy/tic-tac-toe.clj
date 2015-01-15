e58236df45a1cd5b21f5c0579a3567edb050466c
Kyle Lacy <kylewlacy@me.com>
2015-01-14 18:06:01 -0800

    Group tests into contexts
    This also reduces duplication in tests

--- spec/tic_tac_toe/board_spec.clj
+++ spec/tic_tac_toe/board_spec.clj
@@ -3,73 +3,60 @@
             [tic-tac-toe.board :as board]))
 
 (describe "a board"
-  (it "can be created with dimensions"
-    (should= [[nil nil nil]
-              [nil nil nil]
-              [nil nil nil]]
-             (board/empty-board [3 3]))
-    (should= [[nil nil nil nil]
-              [nil nil nil nil]]
-             (board/empty-board [4 2]))
-    (should= [] (board/empty-board [0 0])))
-  (it "can get its dimensions"
-    (should= [3 3] (board/size (board/empty-board [3 3])))
-    (should= [4 2] (board/size (board/empty-board [4 2])))
-    (should= [0 0] (board/size (board/empty-board [0 0]))))
-  (it "can mark cells"
-    (should= [[nil nil nil]
-              [nil :x  nil]
-              [nil nil nil]]
-             (board/mark-cell (board/empty-board [3 3]) [2 2] :x))
-    (should= [[nil :x  nil]
-              [nil :o  nil]
-              [nil nil nil]]
-             (-> (board/empty-board [3 3])
-                 (board/mark-cell [1 2] :x)
-                 (board/mark-cell [2 2] :o))))
-  (it "can get a marker in a cell"
-    (should= :x (-> (board/empty-board [3 3])
-                    (board/mark-cell [1 3] :x)
-                    (board/cell [1 3])))
-    (should= :o (-> (board/empty-board [3 3])
-                    (board/mark-cell [3 3] :x)
-                    (board/mark-cell [2 2] :o)
-                    (board/cell [2 2]))))
-  (it "can retrieve a seq of rows"
-    (should= [[:x  nil :o]
-              [:o  :x  :o]
-              [nil :x  nil]]
-             (-> (board/empty-board [3 3])
-                 (board/mark-cell [1 1] :x)
-                 (board/mark-cell [1 3] :o)
-                 (board/mark-cell [2 2] :x)
-                 (board/mark-cell [2 1] :o)
-                 (board/mark-cell [3 2] :x)
-                 (board/mark-cell [2 3] :o)
-                 board/rows)))
-  (it "can retrieve a seq of columns"
-    (should= [[:x  :o nil]
-              [nil :x :x]
-              [:o  :o nil]]
-             (-> (board/empty-board [3 3])
-                 (board/mark-cell [1 1] :x)
-                 (board/mark-cell [1 3] :o)
-                 (board/mark-cell [2 2] :x)
-                 (board/mark-cell [2 1] :o)
-                 (board/mark-cell [3 2] :x)
-                 (board/mark-cell [2 3] :o)
-                 board/cols)))
-  (it "can retrieve a seq of diagonal"
-    (should= [[:x :x nil]
-              [:o :x nil]]
-             #_[[:x  nil :o]
+  (context "when creating a board"
+    (it "can be created with arbitrary dimensions"
+      (should= [[nil nil nil]
+                [nil nil nil]
+                [nil nil nil]]
+               (board/empty-board [3 3]))
+      (should= [[nil nil nil nil]
+                [nil nil nil nil]]
+               (board/empty-board [4 2]))
+      (should= [] (board/empty-board [0 0])))
+    (it "can get its dimensions"
+      (should= [3 3] (board/size (board/empty-board [3 3])))
+      (should= [4 2] (board/size (board/empty-board [4 2])))
+      (should= [0 0] (board/size (board/empty-board [0 0])))))
+  (context "when getting and setting cells"
+    (with board (board/empty-board [3 3]))
+    (it "can mark cells"
+      (should= [[nil nil nil]
+                [nil :x  nil]
+                [nil nil nil]]
+               (board/mark-cell @board [2 2] :x))
+      (should= [[nil :x  nil]
+                [nil :o  nil]
+                [nil nil nil]]
+               (-> @board
+                   (board/mark-cell [1 2] :x)
+                   (board/mark-cell [2 2] :o))))
+    (it "can get a marker in a cell"
+      (should= :x (-> @board
+                      (board/mark-cell [1 3] :x)
+                      (board/cell [1 3])))
+      (should= :o (-> @board
+                      (board/mark-cell [3 3] :x)
+                      (board/mark-cell [2 2] :o)
+                      (board/cell [2 2])))))
+  (context "when retrieving rows, columns, and diagonals"
+    (with board (-> (board/empty-board [3 3])
+                    (board/mark-cell [1 1] :x)
+                    (board/mark-cell [1 3] :o)
+                    (board/mark-cell [2 2] :x)
+                    (board/mark-cell [2 1] :o)
+                    (board/mark-cell [3 2] :x)
+                    (board/mark-cell [2 3] :o)))
+    (it "can get a seq of rows"
+      (should= [[:x  nil :o]
                 [:o  :x  :o]
                 [nil :x  nil]]
-             (-> (board/empty-board [3 3])
-                 (board/mark-cell [1 1] :x)
-                 (board/mark-cell [1 3] :o)
-                 (board/mark-cell [2 2] :x)
-                 (board/mark-cell [2 1] :o)
-                 (board/mark-cell [3 2] :x)
-                 (board/mark-cell [2 3] :o)
-                 board/diags))))
+               (board/rows @board)))
+    (it "can get a seq of columns"
+      (should= [[:x  :o nil]
+                [nil :x :x]
+                [:o  :o nil]]
+               (board/cols @board)))
+    (it "can get a seq of diagonals"
+      (should= [[:x :x nil]
+                [:o :x nil]]
+               (board/diags @board)))))
