d1529d5d812fda7468bed2c6865a1880b362d723
Kyle Lacy <kylewlacy@me.com>
2015-01-15 16:57:28 -0800

    Add functions to draw a board as a string

--- spec/tic_tac_toe/drawing_spec.clj
+++ spec/tic_tac_toe/drawing_spec.clj
@@ -1,5 +1,6 @@
 (ns tic-tac-toe.drawing-spec
   (:require [speclj.core :refer :all]
+            [tic-tac-toe.board :as board]
             [tic-tac-toe.drawing :as draw]))
 
 (context "when drawing a game of tic-tac-toe"
@@ -15,4 +16,29 @@
     (it "adds proper padding for odd numbers"
       (should= 10 (count (draw/center-str "foo" 10)))
       (should= 9 (count (draw/center-str "foobar" 9)))
-      (should= 9 (count (draw/center-str "foo" 9))))))
+      (should= 9 (count (draw/center-str "foo" 9)))))
+  (describe "drawing a board"
+    (with board (-> (board/empty-board [3 3])
+                    (board/mark-cell [1 1] :x)
+                    (board/mark-cell [2 2] :o)
+                    (board/mark-cell [2 3] :x)
+                    (board/mark-cell [3 1] :o)
+                    (board/mark-cell [3 3] :x)))
+    (around [it]            
+      (binding [draw/*row*        "-"
+                draw/*col*        "|"
+                draw/*row+col*    "+"
+                draw/*markers*    {:x "X", :o "O"}
+                draw/*cell-width* 1]
+        (it)))
+    (it "draws a board's row"
+      (should= "X| | " (draw/row-str (first (board/rows @board))))
+      (should= " |O|X" (draw/row-str (second (board/rows @board))))
+      (should= "O| |X" (draw/row-str (last (board/rows @board)))))
+    (it "draws a full board"
+      (should= (str "X| | " \newline
+                    "-+-+-" \newline
+                    " |O|X" \newline
+                    "-+-+-" \newline
+                    "O| |X")
+               (draw/board-str @board)))))
--- src/tic_tac_toe/drawing.clj
+++ src/tic_tac_toe/drawing.clj
@@ -1,4 +1,12 @@
-(ns tic-tac-toe.drawing)
+(ns tic-tac-toe.drawing
+  (:require [clojure.string :as str]
+            [tic-tac-toe.board :as board]))
+
+(def ^:dynamic *row*        "━")
+(def ^:dynamic *col*        "┃")
+(def ^:dynamic *row+col*    "╋")
+(def ^:dynamic *markers*    {:x "╳", :o "◯"})
+(def ^:dynamic *cell-width* 3)
 
 (defn pad-str [str* left-padding right-padding]
   (let [left-pad  (apply str (repeat left-padding " "))
@@ -11,3 +19,19 @@
         left-padding  (quot padding 2)
         right-padding (- padding left-padding)]
     (pad-str str* left-padding right-padding)))
+
+(defn cell-str* [board-cell]
+  (or (get *markers* board-cell) ""))
+
+(defn cell-str [board-cell]
+  (center-str (cell-str* board-cell) *cell-width*))
+
+(defn row-str [board-row]
+  (str/join *col* (map cell-str board-row)))
+
+(defn board-str [board]
+  (let [[rows cols]    (board/size board)
+        cell-separator (apply str (repeat *cell-width* *row*))
+        row-separator  (str/join *row+col* (repeat cols cell-separator))]
+    (str/join (str \newline row-separator \newline)
+              (map row-str (board/rows board)))))
