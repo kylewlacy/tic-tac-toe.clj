ec5064dc918b322e2882dfe9d32a81208d9a0b93
Kyle Lacy <kylewlacy@me.com>
2015-01-21 15:03:31 -0800

    Move minimax function into `tic-tac-toe.ai`

--- src/tic_tac_toe/ai.clj
+++ src/tic_tac_toe/ai.clj
@@ -1,6 +1,23 @@
 (ns tic-tac-toe.ai
-  (:require [tic-tac-toe.util.minimax :refer [minimax]]
-            [tic-tac-toe.game :as game]))
+  (:require [tic-tac-toe.game :as game]))
+
+(defn minimax
+  ([init-state move-f moves-f terminal?-f value-f max?-f]
+     (minimax init-state move-f moves-f terminal?-f value-f max?-f -1))
+  ([init-state move-f moves-f terminal?-f value-f max?-f max-depth]
+     (letfn [(reduce-moves [moves] (reduce move-f init-state moves))
+             (moves-value [moves] (value-f (reduce-moves moves)))
+             (minimax* [state moves depth]
+               (if (or (terminal?-f state) (zero? depth))
+                 moves
+                 (let [next-minimax #(minimax* (move-f state %)
+                                               (concat moves [%])
+                                               (dec depth))
+                       max?         (max?-f state)
+                       next-moves   (map next-minimax (moves-f state))
+                       select-key   (if max? max-key min-key)]
+                   (apply select-key moves-value next-moves))))]
+       (minimax* init-state [] max-depth))))
 
 (defn pick-move
   ([state]
