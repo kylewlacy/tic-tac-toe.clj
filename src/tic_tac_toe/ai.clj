2e2c5d32d12a5eb20acf8392e3cccf1539b5f5a7
Kyle Lacy <kylewlacy@me.com>
2015-01-22 15:34:03 -0800

    Add `ai/depth-required-for`

--- src/tic_tac_toe/ai.clj
+++ src/tic_tac_toe/ai.clj
@@ -1,5 +1,6 @@
 (ns tic-tac-toe.ai
-  (:require [tic-tac-toe.game :as game]))
+  (:require [tic-tac-toe.board :as board]
+            [tic-tac-toe.game :as game]))
 
 (def unlimited-depth -1)
 
@@ -20,6 +21,16 @@
                        select-key   (if max? max-key min-key)]
                    (apply select-key moves-value next-moves))))]
        (minimax* init-state [] max-depth))))
+
+(defn depth-required-for
+  "Returns the minimum minimax depth required to play a perfect game for a
+   given state (see https://gist.github.com/kylewlacy/867c67b15c298d5204c4
+   for an example of how to compute this)"
+  [state]
+  ;; TODO: Find a formula to compute the depth for any given board size
+  (condp = (board/size (:board state))
+    [3 3] 6
+    unlimited-depth))
 
 (defn pick-move
   ([state]
