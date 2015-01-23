9b71150b4ad3394668d7b43e2bfc4b8bf3dea8a4
Kyle Lacy <kylewlacy@me.com>
2015-01-22 20:24:15 -0800

    Don't use `util.seq` in `board`

--- src/tic_tac_toe/board.clj
+++ src/tic_tac_toe/board.clj
@@ -1,8 +1,7 @@
-(ns tic-tac-toe.board
-  (:require [tic-tac-toe.util.seq :as seq]))
+(ns tic-tac-toe.board)
 
 (defn empty-board [[rows cols]]
-  (repeat cols (repeat rows nil)))
+  (vec (repeat cols (vec (repeat rows nil)))))
 
 (defn size [board]
   (let [cols (count board)
@@ -10,10 +9,10 @@
     [rows cols]))
 
 (defn mark-cell [board [row col] marker]
-  (seq/with-in board [(dec row) (dec col)] marker))
+  (assoc-in board [(dec row) (dec col)] marker))
 
 (defn cell [board [row col]]
-  (seq/fetch-in board [(dec row) (dec col)]))
+  (get-in board [(dec row) (dec col)]))
 
 (defn rows [board]
   board)
@@ -24,10 +23,8 @@
 (defn diags [board]
   (let [[rows cols] (size board)]
     (if (= rows cols)
-      [(map #(seq/fetch-in board [% %])
-            (range rows))
-       (map #(seq/fetch-in board [% (- rows % 1)])
-            (range rows))])))
+      [(map #(get-in board [% %]) (range rows))
+       (map #(get-in board [% (- rows % 1)]) (range rows))])))
 
 (defn cells [board]
   (apply concat board))
